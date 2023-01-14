4d469f5ab814fedee8dc7d779619a478848de860
Davi Mateus <davi.2020109493@unicap.br>
2023-01-14 11:35:15 -0300

    feat: add swagger and start documentation

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerUi from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerSetup from "./swaggerSetup.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSetup));
 
 app.listen(3333, () => console.log("Server is running!"));
